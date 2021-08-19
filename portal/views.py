9090b2fa6db36898b5388d313bca50ae824f0e89
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-08-20 00:47:36 +0530

    Deploy

--- portal/views.py
+++ portal/views.py
@@ -272,7 +272,7 @@ def profile(request,slug):
         user_form = UserForm(request.POST,instance = request.user)
         profile_form = ProfileForm(request.POST,request.FILES,instance = request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
-            user_form.saves()
+            user_form.save()
             profile_form.save()
             messages.success(request,'Profile Updated Successfully')
             return redirect('profile',slug = request.user.profile.slug)
